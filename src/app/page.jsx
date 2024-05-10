6ffd72abe7ab5205bd44960ef1d9fed8f6add9ef
ahmetemincopur <64459050+ahmetemincopur@users.noreply.github.com>
2024-05-10 04:03:22 +0300

    tmdb api key added to .env.local file. Data fetch functions added to page.jsx. Fetched data passed to Card component. Fetched data have  used to get posters, titles, rates, and release dates. Unnecessary parts deleted from Card component. h1 added to show which category shown

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import Card from '@/components/Card'
+import React from 'react';
+import Card from '@/components/Card';
 
-const Page = () => {
+const API_KEY = process.env.API_KEY;
+
+const Page = async() => {
+
+
+        const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`,{next:{revalidate: 10000}});
+        if(!res.ok){
+            throw new Error('Failed to fetch data')
+        }
+        const data = await res.json();
+        const results = data.results;
   return (
-    <div className=''>
-        <Card/>
-    </div>
-  )
-}
+    <>
+        <div className="m-10 w-1/6">
+        <h1 className="font-bold text-2xl text-end">Popular Movies</h1>
+        </div>
+           
+            <div className="flex flex-row gap-10 mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl flex-wrap justify-center">
+                {results.map(movie => (
+                <Card key={movie.id} movie={movie} />
+                ))}
+            </div>
 
-export default Page
+    </>
+
+  );
+};
+
+export default Page;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,24 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({movie}) => {
   return (
-    <div class="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+    <div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl max-w-lg">
   <div
-    class="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
+    className="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
     <img
-      src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+      src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path || movie?.poster_path}`}
       alt="card-image" />
   </div>
-  <div class="p-6">
-    <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-      UI/UX Review Check
+  <div className="p-6">
+    <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+      <span>{movie.title}</span>
     </h5>
-    <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-      The place is close to Barceloneta Beach and bus stop just 2 min by walk
-      and near to "Naviglio" where you can enjoy the main night life in
-      Barcelona.
-    </p>
+    <div className="p-6 flex justify-between items-center">
+        <span>{movie.release_date}</span>
+        <span>{movie.vote_average}</span>
+    </div>
   </div>
-</div>  
+</div>
   )
 }
 
